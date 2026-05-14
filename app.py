07b2b088f6897b027132dfe704010d6ea8438961
Martin Prediger <mprediger@fi.uba.ar>
2026-05-14 11:48:04 -0300

    Estructura inicial de carpetas y front-end

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route('/menu')
+def pagina_menu(): 
+    return render_template('menu.html')
+
+
+@app.route('/nosotros')
+def pagina_nosotros():
+    return render_template('nosotros.html')
+
+
+@app.route('/reservas')
+def pagina_reservas():
+    return render_template('reserva.html')
+
+
+@app.route('/clientes')
+def pagina_clientes():
+    return render_template('clientes.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
