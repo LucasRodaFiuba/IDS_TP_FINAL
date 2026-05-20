dce353046af4165091122dd008c49cea86508e79
Martin Prediger <mprediger@fi.uba.ar>
2026-05-20 14:20:57 -0300

    Avances del front
    Co-authored-by: nachin-gulis <Igonzalezd@fi.uba.ar>

--- app.py
+++ app.py
@@ -27,5 +27,14 @@ def pagina_clientes():
     return render_template('clientes.html')
 
 
+@app.route('/iniciar_sesion')
+def iniciar_sesion():
+    return render_template('iniciar_sesion.html')
+
+@app.route('/admin')
+def pagina_admin():
+    return render_template('admin.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
