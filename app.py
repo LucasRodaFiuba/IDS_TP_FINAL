3d9216db449a6ee08cbfc794e88a79e24115183c
nachin-gulis <igonzalezd@fi.uba.ar>
2026-05-23 12:46:48 -0300

    Avances del front - parte 2
    Co-authored-by: mprediger-240 <mprediger@fi.uba.ar>

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -34,6 +35,90 @@ def iniciar_sesion():
 @app.route('/admin')
 def pagina_admin():
     return render_template('admin.html')
+
+@app.route('/admin/reservas')
+def admin_reservas():
+    return render_template('admin_reservas.html')
+
+@app.route('/admin/clientes')
+def admin_clientes():
+    return render_template('admin_clientes.html')
+
+
+
+@app.route('/admin/menu/entradas', methods=['POST'])
+def agregar_entrada():
+    nombre = request.form['nombre']
+    descripcion = request.form['descripcion']
+    precio = request.form['precio']
+    ingredientes = request.form['ingredientes']
+
+@app.route('/admin/menu/plato', methods=['POST'])
+def agregar_plato():
+    nombre = request.form['nombre']
+    descripcion = request.form['descripcion']
+    precio = request.form['precio']
+    ingredientes = request.form['ingredientes']
+
+@app.route('/admin/menu/postre', methods=['POST'])
+def agregar_postre():
+    nombre = request.form['nombre']
+    descripcion = request.form['descripcion']
+    precio = request.form['precio']
+    ingredientes = request.form['ingredientes']
+
+@app.route('/admin/menu/bebida', methods=['POST'])
+def agregar_bebida():
+    nombre = request.form['nombre']
+    descripcion = request.form['descripcion']
+    precio = request.form['precio']
+    ingredientes = request.form['ingredientes']
+    
+@app.route('/admin/menu/modificar', methods=['UPDATE'])
+def modificar_objeto():
+    nombre = request.form['nombre']
+    descripcion = request.form['descripcion']
+    precio = request.form['precio']
+    ingredientes = request.form['ingredientes']
+
+@app.route('/admin/menu/eliminar', methods=['DELETE'])
+def eliminar_objeto():
+    nombre = request.form['nombre']
+
+@app.route('/admin/reservas/agregar', methods=['POST'])
+def agregar_reserva():
+    nombre_cliente = request.form['nombre_cliente']
+    fecha_hora = request.form['fecha_hora']
+    cantidad_personas = request.form['cantidad_personas']
+
+@app.route('/admin/reservas/modificar', methods=['UPDATE'])
+def modificar_reserva():
+    nombre_cliente = request.form['nombre_cliente']
+    fecha_hora_reserva = request.form['fecha_hora_reserva']
+    nueva_fecha_hora = request.form['nueva_fecha_hora']
+
+@app.route('/admin/reservas/eliminar', methods=['DELETE'])
+def eliminar_reserva():
+    nombre_cliente = request.form['nombre_cliente']
+    fecha_hora = request.form['fecha_hora']
+
+
+@app.route('/admin/usuarios/agregar', methods=['POST'])
+def agregar_usuario():
+    nombre_usuario = request.form['nombre_usuario']
+    correo_electronico = request.form['correo_electronico']
+    contrase単a = request.form['contrase単a']
+
+@app.route('/admin/usuarios/modificar', methods=['UPDATE'])
+def modificar_usuario():
+    nombre_usuario = request.form['nombre_usuario']
+    nuevo_nombre_usuario = request.form['nuevo_nombre_usuario']
+    nuevo_correo_electronico = request.form['nuevo_correo_electronico']
+    nueva_contrase単a = request.form['nueva_contrase単a']
+
+@app.route('/admin/usuarios/eliminar', methods=['DELETE'])
+def eliminar_usuario():
+    nombre_usuario = request.form['nombre_usuario']
 
 
 if __name__ == "__main__":
