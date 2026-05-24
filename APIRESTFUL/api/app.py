61390cc2a00bf636c85315c0592a8193339c7fa6
Federico <programacion.fede@gmail.com>
2026-05-24 19:33:52 -0300

    Principalmente , añado el blueprint para los endpoints de reservas

--- APIRESTFUL/api/app.py
+++ APIRESTFUL/api/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from .constantes import BASE_URL
 #agregar importaciones para los blueprints u otras cosas importantes
+from .routes.reservas import reservas_bp
+
 
 app =Flask(__name__)
 
 #agregar blueprints
-#...
+app.register_blueprint(reservas_bp, url_prefix=BASE_URL)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
