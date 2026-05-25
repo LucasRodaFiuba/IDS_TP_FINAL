0302de8516577ff5a04b8fd3e6d373595f36d3a1
Federico <programacion.fede@gmail.com>
2026-05-24 23:53:40 -0300

    Agrego en routes todo lo relacionado con el endpoint reservas/disponibilidad (URL)

--- APIRESTFUL/api/routes/reservas.py
+++ APIRESTFUL/api/routes/reservas.py
@@ -1,10 +1,35 @@
 from flask import Blueprint, jsonify, request
 from ..utils import construir_error_api
 from ..services.reservas import (
+    consultar_disponibilidad,
     crear_reserva
 )
 
 reservas_bp = Blueprint('reservas', __name__)
+
+@reservas_bp.route('/reservas/disponibilidad', methods = ['GET'])
+def obtener_turnos_disponibles():
+    #Obtengo los dos paŕametros completados por el usuario
+    fecha = request.args.get('fecha')
+    comensales = request.args.get('comensales',type=int)
+
+    try:
+        turnos_disponibles = consultar_disponibilidad(fecha,comensales)
+    except ValueError as e:
+        status = e.args[1] if len(e.args) > 1 else 400
+        return jsonify(e.args[0]), status
+    except Exception as e:
+        return jsonify({
+            'errors': [
+                {
+                    'code': 'internal.server.error',
+                    'message': str(e),
+                    'description': 'Ocurrio un error inesperado, estoy en routes'
+                }
+            ]
+        }), 500
+
+    return jsonify(turnos_disponibles), 200
 
 @reservas_bp.route('/reservas' , methods = ['POST'])
 def agregar_reservar():
