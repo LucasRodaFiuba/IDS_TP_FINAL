8e9ddfe69c7d149988ad4dad00be60e1c13f77dc
Federico <programacion.fede@gmail.com>
2026-05-26 01:55:30 -0300

    Utilizo db_funciones.py en vez de db.py que era donde tenia las funciones SQL, lo cambio para que no hayan conflictos por como esta utilizada en la rama desarrollo-back

--- APIRESTFUL/api/services/reservas.py
+++ APIRESTFUL/api/services/reservas.py
@@ -1,7 +1,7 @@
 import logging
 from ..utils import construir_error_api
 from ..validators.reservas import validar_body_nueva_reserva,validar_parametros
-from .. import db
+from .. import db_funciones
 from ..constantes import HORARIOS_PARA_RESERVAR
 
 #UTIL PARA LO DEL QR
@@ -22,7 +22,7 @@ def consultar_disponibilidad(fecha,comensales):
     fecha = datos['fecha']
     comensales = datos['comensales']
 
-    mesas = db.obtener_mesas_por_capacidad(comensales)
+    mesas = db_funciones.obtener_mesas_por_capacidad(comensales)
     print("MESAS:", mesas)
     turnos_disponibles = []
 
@@ -31,7 +31,7 @@ def consultar_disponibilidad(fecha,comensales):
     for hora in HORARIOS_PARA_RESERVAR:
         for mesa in mesas:
             print("MESA:", mesa)
-            if not db.mesa_ocupada(mesa['numero_mesa'],fecha,hora):
+            if not db_funciones.mesa_ocupada(mesa['numero_mesa'],fecha,hora):
                 #No está ocupada
                 turnos_disponibles.append(hora)
                 break #en caso de que se repita horario
@@ -56,7 +56,7 @@ def crear_reserva(body):
     # 1.Obtengo las mesas posibles , es decir
     # podrían entrar esa cantidad de comensales
     # (no significa que esten libres)
-    mesas = db.obtener_mesas_por_capacidad(datos['comensales'])
+    mesas = db_funciones.obtener_mesas_por_capacidad(datos['comensales'])
     mesa_asignada = None
 
     if len(datos["hora"]) == 5:
@@ -64,7 +64,7 @@ def crear_reserva(body):
 
     # 2.Busco si hay alguna mesa libre. (de las que están disponibles)
     for mesa in mesas:
-        if not db.mesa_ocupada(mesa['numero_mesa'], datos['fecha'], datos['hora']):
+        if not db_funciones.mesa_ocupada(mesa['numero_mesa'], datos['fecha'], datos['hora']):
             mesa_asignada = mesa
             break
 
@@ -77,7 +77,7 @@ def crear_reserva(body):
         ),409)
 
     #4. Obtengo el id del usuario
-    id_usuario = db.obtener_usuario_por_email(datos['email'])
+    id_usuario = db_funciones.obtener_usuario_por_email(datos['email'])
 
     #Muestro mensaje de error sino existe usuario
     if not id_usuario:
@@ -91,7 +91,7 @@ def crear_reserva(body):
     token = str(uuid.uuid4())
 
     # 6. insertar reserva (debe devolver el id_reserva)
-    id_reserva = db.añadir_reserva(
+    id_reserva = db_funciones.añadir_reserva(
         id_usuario= id_usuario,
         numero_mesa=mesa_asignada['numero_mesa'],
         fecha=datos['fecha'],
@@ -137,13 +137,13 @@ def cambiar_reserva(id_reserva,body):
     """
     datos = validar_body_nueva_reserva(body)
 
-    id_usuario = db.obtener_id_usuario(id_reserva)
+    id_usuario = db_funciones.obtener_id_usuario(id_reserva)
 
     if not id_usuario:
         raise ValueError("Reserva no existe",404)
 
-    db.update_reserva(id_reserva, datos)
-    db.update_usuario(id_usuario, datos)
+    db_funciones.update_reserva(id_reserva, datos)
+    db_funciones.update_usuario(id_usuario, datos)
 
     return {
         "message": "Reserva actualizada correctamente",
@@ -156,7 +156,7 @@ def cancelar_reserva_service(id_reserva):
     """
     #el id_reserva ya viene validado
 
-    filas_afectadas = db.cancelar_reserva(id_reserva)
+    filas_afectadas = db_funciones.cancelar_reserva(id_reserva)
 
     #En caso de que el id_reserva no exista
     if filas_afectadas == 0:
@@ -172,7 +172,7 @@ def cancelar_reserva_service(id_reserva):
 #Services para todo el tema del QR
 def validar_reserva_service(token):
     print("TOKEN:", token)
-    reserva = db.buscar_reserva_por_token(token)
+    reserva = db_funciones.buscar_reserva_por_token(token)
     print("RESERVA:", reserva)
 
     if not reserva:
@@ -188,6 +188,6 @@ def validar_reserva_service(token):
             description="El email no está registrado en la base de datos"))
 
     #Actualizo el estado en confirmada
-    db.actualizar_estado_reserva(token, "confirmada")
+    db_funciones.actualizar_estado_reserva(token, "confirmada")
 
     return {"message": "Reserva confirmada"}
