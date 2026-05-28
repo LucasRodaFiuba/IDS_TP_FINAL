89b4922615fd18780d51ecb9cb7eeb2d65fea5e3
Federico <programacion.fede@gmail.com>
2026-05-28 00:50:39 -0300

    Termino la logica que permite hacer la conexion entre el back y el front de reservas

--- services/reservas.py
+++ services/reservas.py
@@ -9,32 +9,25 @@ def enviar_reserva(datos):
     #Asumo que está bien validado el contenido del formulario.
     nombre = datos.get('nombre_cliente')
     email = datos.get('correo_electronico')
-    #telefono = datos['telefono']
+    telefono = datos.get('telefono-cliente')
     fecha = datos.get('fecha_reserva')
     hora = datos.get('horario_reserva')
     comensales = datos.get('cantidad_personas')
-    #servicios_extras_id = datos['servicios_extras_id']
+    servicios_extras_id = datos.get('servicios_extras')
+
+    #convierto a entero
+    servicios_extras_id_enteros = []
+
+    for servicio in servicios_extras_id:
+        conversion = int(servicio)
+        servicios_extras_id_enteros.append(conversion)
 
     try:
-        print("PAYLOAD FINAL:", {
-            'nombre_cliente': nombre,
-            'email': email,
-            'telefono': "+541112344321",
-            'fecha': fecha,
-            'hora': hora,
-            'comensales': comensales,
-            'servicios_extras_id': [],
-            'notas_especiales': "su"
-        })
-
-        response = requests.post(f'{API_BASE_URL}/reservas',json={'nombre_cliente': nombre,'email': email,'telefono':"+541112344321", 'fecha': fecha, 'hora': hora,'comensales' : int(comensales),"servicios_extras_id":[],"notas_especiales":"su"},timeout=10,)
+        response = requests.post(f'{API_BASE_URL}/reservas',json={'nombre_cliente': nombre,'email': email,'telefono':telefono, 'fecha': fecha, 'hora': hora,'comensales' : int(comensales),"servicios_extras_id":servicios_extras_id_enteros},timeout=10,)
 
         if response.status_code == 201:
             return {'ok': True}
         else:
-            print("STATUS:", response.status_code)
-            print("BODY:", response.text)
-
             return {
                 'ok': False,
                 'errores': [
