ca0864b1d97e12b06e3aa8c24546ae54e1aacd6a
Federico <programacion.fede@gmail.com>
2026-05-27 23:35:24 -0300

    Agrego lógica de negocio que conecta con el backend para reservas

--- services/reservas.py
+++ services/reservas.py
@@ -5,36 +5,49 @@ from constants import API_BASE_URL
 
 logger = logging.getLogger(__name__)
 
-def obtener_reservas():
-    reservas = []
+def enviar_reserva(datos):
+    #Asumo que está bien validado el contenido del formulario.
+    nombre = datos.get('nombre_cliente')
+    email = datos.get('correo_electronico')
+    #telefono = datos['telefono']
+    fecha = datos.get('fecha_reserva')
+    hora = datos.get('horario_reserva')
+    comensales = datos.get('cantidad_personas')
+    #servicios_extras_id = datos['servicios_extras_id']
 
     try:
-        response = requests.get(f'{API_BASE_URL}/reservas', timeout=10)
+        print("PAYLOAD FINAL:", {
+            'nombre_cliente': nombre,
+            'email': email,
+            'telefono': "+541112344321",
+            'fecha': fecha,
+            'hora': hora,
+            'comensales': comensales,
+            'servicios_extras_id': [],
+            'notas_especiales': "su"
+        })
 
-        if response.status_code == 200:
-            reservas = response.json()
-
-    except requests.exceptions.ConnectionError:
-        logger.error(f"No se pudo conectar con la API en {API_BASE_URL}")
-        return {'errores': ['No se pudo conectar con el servidor. Verifica que la API esté corriendo.']}
-
-    except Exception:
-        return {'errores': ['Ocurrió un error inesperado.']}
-    return reservas
-
-def enviar_reserva(fecha: str, hora: str, personas: int, nombre: str):
-
-    try:
-        response = requests.post(f'{API_BASE_URL}/reservas',json={'fecha': fecha,'hora': hora,'personas': personas, 'nombre': nombre},timeout=10,)
+        response = requests.post(f'{API_BASE_URL}/reservas',json={'nombre_cliente': nombre,'email': email,'telefono':"+541112344321", 'fecha': fecha, 'hora': hora,'comensales' : int(comensales),"servicios_extras_id":[],"notas_especiales":"su"},timeout=10,)
 
         if response.status_code == 201:
             return {'ok': True}
         else:
-            return {'errores': ['El servidor no pudo registrar la reserva.']}
+            print("STATUS:", response.status_code)
+            print("BODY:", response.text)
+
+            return {
+                'ok': False,
+                'errores': [
+                f"Error API {response.status_code}: {response.text}"
+            ]
+        }
         
     except requests.exceptions.ConnectionError:
         logger.error(f"No se pudo conectar con la API en {API_BASE_URL}")
         return {'errores': ['No se pudo conectar con el servidor. Verifica que la API esté corriendo.']}
 
-    except Exception:
-        return {'errores': ['Ocurrió un error inesperado.']}
+    except Exception as e:
+        print(e)
+        print(response.status_code)
+        print(response.text)
+        return {'errores': [str(e)]}
