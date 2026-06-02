aa7e73eda4b504a4ba6446cb97ce1e5e85e47cd5
Lucas <lroda@fi.uba.ar>
2026-06-02 00:00:13 -0300

    Arreglados errores de distribucion y de estructura para el back del dashboard, y un poco del back general (app.py del back), cambie el dashboard.py de services para que no usara clases, falta hacer la corrección que señaló el tutor y terminar de integrar el front con estas correcciones

--- APIRESTFUL/api/app.py
+++ APIRESTFUL/api/app.py
@@ -8,25 +8,29 @@ from .routes.servicios import servicios_extra_bp
 from .routes.auth import auth_bp
 from .routes.usuarios import usuarios_bp
 from .routes.menu import menu_bp
+from .routes.dashboard import dashboard_bp
 
 
 
 # Buscar módulos en la carpeta donde esté app.py
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from api.routes import registrar_rutas
+# Registrar los endpoints distribuidos en la carpeta routes
+def registrar_rutas(app):
+    #agregar blueprints
+    app.register_blueprint(reservas_bp, url_prefix=BASE_URL)
+    app.register_blueprint(servicios_extra_bp)
+    app.register_blueprint(auth_bp, url_prefix=BASE_URL)
+    app.register_blueprint(usuarios_bp, url_prefix=BASE_URL)
+    app.register_blueprint(menu_bp, url_prefix=BASE_URL)
+    app.register_blueprint(dashboard_bp, url_prefix=BASE_URL)
+
 
 app = Flask(__name__)
 app.json.sort_keys = False  #no ordena alfabaticamente 
 
-# Registrar los endpoints distribuidos en la carpeta routes
 registrar_rutas(app)
-#agregar blueprints
-app.register_blueprint(reservas_bp, url_prefix=BASE_URL)
-app.register_blueprint(servicios_extra_bp)
-app.register_blueprint(auth_bp, url_prefix=BASE_URL)
-app.register_blueprint(usuarios_bp, url_prefix=BASE_URL)
-app.register_blueprint(menu_bp, url_prefix=BASE_URL)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
